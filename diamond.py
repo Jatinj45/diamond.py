70c026b576db620b3042729b8e99f45567ef9627
Jatin Thapa <thapajatin45@gmail.com>
2019-07-28 08:56:47 +0545

    Initialize and Add diamond.py

---
+++ diamond.py
@@ -0,0 +1,13 @@
+spaces = 3
+while(spaces >= 0):
+    if(spaces != 3):        
+        print(" " * spaces + "*" ,end = "")
+    else:
+        print(" " * spaces + "*")
+    i = 3 - spaces 
+    spaces = spaces - 1
+    innerspace  = i * 2 - 1
+    if i == 0:
+        continue
+    print(" " * innerspace + "*")
+    
